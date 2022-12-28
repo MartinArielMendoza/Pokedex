ec83b45c109adf3c5bfd78a8e51ca07532aaa34c
MartinArielMendoza <marttx2022@gmail.com>
2022-12-27 23:10:31 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import CartWidget from './components/CartWidget/CartWidget';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Navbar/>
+      <CartWidget/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <nav className = 'Navbar'>
+            <h1>Pokedex</h1>
+            <div clasName = 'botones'>
+                <button>Pokemones</button>
+                <button>Tipos</button>
+                <button>Generaciones</button>
+                <button>Capturados</button>
+
+            </div>
+        </nav>
+    )
+    }
+    export default Navbar
